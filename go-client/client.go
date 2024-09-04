2817a0b003794d6480a682d8ed93c31a8b6fcc2a
zhangcunli <zhangcl@tuya.com>
2024-09-04 18:47:05 +0800

    init

---
+++ go-client/client.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"dubbo.apache.org/dubbo-go/v3/client"
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	"dubbo.apache.org/dubbo-go/v3/protocol/triple/triple_protocol"
+	"gitlab.com/zhangcl/dubbo-stream1/proto/greetmsg"
+)
+
+func main() {
+	testTripleStream()
+}
+
+func testTripleStream() error {
+	triAddr := "tri://127.0.0.1:20000"
+	cli, err := client.NewClient(
+		client.WithClientURL(triAddr),
+	)
+	if err != nil {
+		fmt.Printf(">>>testTripleStream NewClient err:%s\n", err)
+		panic(err)
+	}
+
+	key := strings.ToLower("fusion-context")
+	fusionContext := make(map[string]interface{})
+	value, _ := json.Marshal(newFusionContext())
+	fusionContext[key] = string(value)
+	ctx := context.WithValue(context.Background(), constant.AttachmentKey, fusionContext)
+
+	interfaceName := "greetsvr.GreetService"
+	greetService_ClientInfo := client.ClientInfo{
+		InterfaceName: interfaceName,
+		MethodNames:   []string{"GreetStream", "GreetServerStream"},
+		//ConnectionInjectFunc: func(dubboCliRaw interface{}, conn *client.Connection) {
+		//	//dubboCli := dubboCliRaw.(*GreetServiceImpl)
+		//	dubboCli.conn = conn
+		//},
+	}
+
+	conn, err := cli.DialWithInfo(interfaceName, &greetService_ClientInfo)
+	if err != nil {
+		fmt.Printf(">>>testTripleStream DialWithInfo err:%s\n", err)
+		return err
+	}
+
+	methodName := "GreetServerStream"
+	svrStream, err := conn.CallServerStream(ctx, &greetmsg.GreetServerStreamRequest{Name: "Triple.stream.client"}, methodName)
+	if err != nil {
+		fmt.Printf(">>>testTripleStream CallServerStream err:%s\n", err)
+		return err
+	}
+
+	fmt.Printf("\n")
+	stream := svrStream.(*triple_protocol.ServerStreamForClient)
+	msg := new(greetmsg.GreetServerStreamResponse)
+	for stream.Receive(msg) {
+		fmt.Printf("TRIPLE server stream resp Greeting: %s\n", msg.Greeting)
+	}
+	if stream.Err() != nil {
+		return err
+	}
+	if err := stream.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type FusionContext struct {
+	ClientInfo ClientInfo `json:"clientInfo"`
+	RequestInfo RequestInfo `json:"requestInfo"`
+}
+
+type ClientInfo struct {
+	ClientId *string `json:"clientId,omitempty"`
+	BizType  *int    `json:"bizType,omitempty"`
+	AppId    *int    `json:"appId,omitempty"`
+}
+
+type RequestInfo struct {
+	Lang          string             `json:"lang"`
+	NeutralDomain bool               `json:"neutralDomain"`
+	BizData       *map[string]string `json:"bizData,omitempty"`
+}
+
+func newFusionContext() *FusionContext {
+	clientId := "clientId_0001"
+	bizType := 10
+	clientInfo := ClientInfo{
+		ClientId: &clientId,
+		BizType:  &bizType,
+	}
+
+	bizData := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+	}
+	requestInfo := RequestInfo{
+		Lang:          "zh",
+		NeutralDomain: true,
+		BizData:       &bizData,
+	}
+	fusionContext := FusionContext{
+		ClientInfo:  clientInfo,
+		RequestInfo: requestInfo,
+	}
+
+	return &fusionContext
+}
---
+++ go-server/server.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	"dubbo.apache.org/dubbo-go/v3/common/constant"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	"dubbo.apache.org/dubbo-go/v3/protocol"
+	"dubbo.apache.org/dubbo-go/v3/server"
+	"gitlab.com/zhangcl/dubbo-stream1/proto/greetmsg"
+	"gitlab.com/zhangcl/dubbo-stream1/proto/greetsvr"
+)
+
+func main() {
+	srv, err := server.NewServer(
+		server.WithServerProtocol(
+			protocol.WithPort(20000),
+		),
+	)
+	if err != nil {
+		fmt.Printf(">>>main new server failed, err:%s\n", err)
+		panic(err)
+	}
+
+	if err := greetsvr.RegisterGreetServiceHandler(srv, &GreetTripleServer{}); err != nil {
+		fmt.Printf(">>>main regist server failed, err:%s\n", err)
+		panic(err)
+	}
+	if err := srv.Serve(); err != nil {
+		fmt.Printf("Server err:%s\n", err)
+	}
+}
+
+type GreetTripleServer struct {
+}
+
+func (srv *GreetTripleServer) GreetStream(ctx context.Context, stream greetsvr.GreetService_GreetStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return fmt.Errorf("triple BidiStream recv error: %s", err)
+		}
+
+		msg := req.Name
+		if err := stream.Send(&greetmsg.GreetStreamResponse{Greeting: msg}); err != nil {
+			fmt.Printf(">>>GreetStream Send msg:%s\n", msg)
+			return fmt.Errorf("triple BidiStream send error: %s", err)
+		}
+	}
+	return nil
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+func (srv *GreetTripleServer) GreetServerStream(ctx context.Context, req *greetmsg.GreetServerStreamRequest, stream greetsvr.GreetService_GreetServerStreamServer) error {
+	key := constant.AttachmentKey
+	value, ok := ctx.Value(key).(map[string]interface{})
+	if !ok {
+		fmt.Printf(">>>GreetServerStream ctx key:%s not exist!!!\n", key)
+	} else {
+		//values, _ := json.Marshal(value)
+		//fmt.Printf(">>>ctx key:%s, value:%s\n", key, values)
+
+		key1 := strings.ToLower("fusion-context")
+		fValue, _ := value[key1]
+		fmt.Printf(">>>ctx attachement key:%s, value:%s\n", key1, fValue)
+	}
+
+	for i := 0; i < 5; i++ {
+		message := fmt.Sprintf("Hello, %s!", req.Name)
+		fmt.Printf(">>>GreetServerStream send: %s\n", message)
+		if err := stream.Send(&greetmsg.GreetServerStreamResponse{Greeting: message}); err != nil {
+			return fmt.Errorf("triple ServerStream send err: %s", err)
+		}
+	}
+	return nil
+}
